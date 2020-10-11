d9d4e82cc947f40915454532e4a7bad70bc0c630
huangkairan <18757128360@163.com>
2020-10-11 21:02:07 +0800

    style(app-header):设置头部样式

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,14 +1,21 @@
 import React, { memo } from "react";
 import { NavLink } from "react-router-dom";
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 
 export default memo(function KBAppHeader() {
   return (
-    <div>
-      <h2>
-        <NavLink to="/">发现音乐</NavLink>
-        <NavLink to="/mine">我的音乐</NavLink>
-        <NavLink to="/friend">朋友</NavLink>
-      </h2>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <NavLink to="/"></NavLink>
+        </HeaderLeft>
+        <HeaderRight>
+          <NavLink to="/">发现音乐</NavLink>
+          <NavLink to="/mine">我的音乐</NavLink>
+          <NavLink to="/friend">朋友</NavLink>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
---
+++ src/components/app-header/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+export const HeaderWrapper = styled.div`
+  height: 75px;
+  color: white;
+  background-color: #242424;
+  .content {
+    height: 70px;
+    background-color: blue;
+    display: flex;
+    justify-content: space-between;
+  }
+  .divider {
+    height: 5px;
+    background-color: #c20c0c;
+  }
+`;
+
+export const HeaderLeft = styled.div``;
+export const HeaderRight = styled.div``;
