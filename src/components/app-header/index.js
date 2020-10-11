a7516c5dfeefb6b07bd9a68ac5ebfe0acf18a733
huangkairan <18757128360@163.com>
2020-10-11 20:44:40 +0800

    feat(app-header):增加NavLInk

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,9 +1,14 @@
 import React, { memo } from "react";
+import { NavLink } from "react-router-dom";
 
 export default memo(function KBAppHeader() {
   return (
     <div>
-      <h2>KBAppHeader</h2>
+      <h2>
+        <NavLink to="/">发现音乐</NavLink>
+        <NavLink to="/mine">我的音乐</NavLink>
+        <NavLink to="/friend">朋友</NavLink>
+      </h2>
     </div>
   );
 });
