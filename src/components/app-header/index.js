70a47d976fe2bd70932b814f35461982608ed887
huangkairan <18757128360@163.com>
2020-10-11 21:29:36 +0800

    feat(app-header):NavLink完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,19 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React, { memo } from "react";
 import { NavLink } from "react-router-dom";
+import { headerLinks } from "@/common/local-data";
 import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 
 export default memo(function KBAppHeader() {
+  const showSelectItem = (item,index)=>{
+    if(index<3){
+      return (
+      <NavLink to={item.link} exact>
+        {item.title}
+        <i className="sprite_01 icon"></i>
+      </NavLink>
+      )
+    }else{
+      return (
+      <a href={item.link}>{item.title}</a>
+      )
+    }
+  }
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
         <HeaderLeft>
           <a href="#/" className="logo sprite_01"></a>
+          <div className="select-list">
+            {
+              headerLinks.map((item,index)=>{
+                return (
+                  <div key={item.title} 
+                  className="select-item">
+                    {showSelectItem(item,index)}
+                  </div>
+                )
+              })
+            }
+          </div>
         </HeaderLeft>
         <HeaderRight>
-          <NavLink to="/">发现音乐</NavLink>
-          <NavLink to="/mine">我的音乐</NavLink>
-          <NavLink to="/friend">朋友</NavLink>
+          
         </HeaderRight>
       </div>
       <div className="divider"></div>
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 export const HeaderWrapper = styled.div`
   height: 75px;
   color: white;
+  font-size:14px;
   background-color: #242424;
   .content {
     height: 70px;
@@ -15,11 +16,57 @@ export const HeaderWrapper = styled.div`
 `;
 
 export const HeaderLeft = styled.div`
+  display: flex;
   .logo {
     display: block;
     width: 176px;
     height: 69px;
     background-position: 0 0;
+  }
+  .select-list {
+    display: flex;
+    line-height: 70px;
+    
+    .select-item {
+      position: relative;
+      a {
+        display: block;
+        padding: 0 20px;
+        color: #ccc;
+      }
+
+      :last-of-type a {
+        position: relative;
+        ::after {
+          position: absolute;
+          content: "";
+          width: 28px;
+          height: 19px;
+          //css中引入精灵图要像引入模块一样
+          background-image: url(${require("@/assets/img/sprite_01.png")});
+          background-position: -190px 0;
+          top: 20px;
+          right: -15px;
+        }
+      }
+
+      &:hover a, a.active {
+        color: #fff;
+        background: #000;
+        text-decoration: none;
+      }
+      
+      .active .icon {
+        position: absolute;
+        display: inline-block;
+        width: 12px;
+        height: 7px;
+        bottom: -1px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        background-position: -226px 0;
+      }
+    }
   }
 `;
 export const HeaderRight = styled.div``;
