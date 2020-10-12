1294fe9afee20f086f00bf2fc7a2ddbcd5f011c8
huangkairan <18757128360@163.com>
2020-10-12 16:22:58 +0800

    chore:axios

---
+++ src/services/request.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { BASE_URL, TIMEOUT } from "./config";
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: TIMEOUT,
+});
+instance.interceptors.request.use(
+  (config) => {
+    console.log("请求被拦截");
+    return config;
+  },
+  (err) => {}
+);
+instance.interceptors.response.use(
+  (res) => {
+    return res.data;
+  },
+  (err) => {
+    if (err && err.response) {
+      switch (err.response.status) {
+        case 400:
+          console.log("请求错误");
+          break;
+        case 401:
+          console.log("未授权");
+          break;
+        default:
+          console.log("err");
+      }
+    }
+    return err;
+  }
+);
+export default instance;
