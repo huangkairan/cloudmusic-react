bed3ebfd37d46fe51f9cbea8ba2ae0e1749ddc78
huangkairan <18757128360@163.com>
2020-10-12 16:40:02 +0800

    feat:redux配置

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { memo } from "react";
 import { renderRoutes } from "react-router-config";
-
+import { Provider } from "react-redux";
 import routes from "./router";
 
 import KBAppHeader from "components/app-header";
 import KBAppFooter from "components/app-footer";
 import { HashRouter } from "react-router-dom";
-
+import store from "./store";
 export default memo(function App() {
   return (
-    <HashRouter>
-      <KBAppHeader />
-      {renderRoutes(routes)}
-      <KBAppFooter />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+          <KBAppHeader />
+          {renderRoutes(routes)}
+          <KBAppFooter />
+        </HashRouter>
+    </Provider>
   );
 });
---
+++ src/store/reducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+
+const cReducer = combineReducers({});
+export default cReducer;
