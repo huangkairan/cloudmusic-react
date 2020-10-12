2237daef9b2768ec8345864c6776feefd2d6d23a
huangkairan <18757128360@163.com>
2020-10-12 15:56:39 +0800

    feat:NavLink增加

--- src/common/local-data.js
+++ src/common/local-data.js
@@ -65,3 +65,32 @@ export const footerImages = [
     link: "https://music.163.com/uservideo#/plan"
   }
 ]
+
+
+// discover中的数据
+export const discoverMenu = [
+  {
+    title: "推荐",
+    link: "/discover/recommend"
+  },
+  {
+    title: "排行榜",
+    link: "/discover/ranking"
+  },
+  {
+    title: "歌单",
+    link: "/discover/songs"
+  },
+  {
+    title: "主播电台",
+    link: "/discover/djradio"
+  },
+  {
+    title: "歌手",
+    link: "/discover/artist"
+  },
+  {
+    title: "新碟上架",
+    link: "/discover/album"
+  },
+]
--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,5 +1,22 @@
 import React, { memo } from "react";
 
+import { DiscoverWrapper, TopMenu } from "./style";
+import { discoverMenu } from "@/common/local-data";
+import { NavLink } from "react-router-dom";
 export default memo(function KBDiscover() {
-  return <div>KBDiscover</div>;
+  return (
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {discoverMenu.map((item, index) => {
+            return (
+              <div className="item" key={item.title}>
+                <NavLink to={item.link}>{item.title}</NavLink>
+              </div>
+            );
+          })}
+        </TopMenu>
+      </div>
+    </DiscoverWrapper>
+  );
 });
