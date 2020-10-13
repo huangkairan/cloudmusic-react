d2181b91839d5664bc11455c6938f8d10629e1b1
huangkairan <18757128360@163.com>
2020-10-13 20:30:10 +0800

    feat(hot-recommend):数据展示

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -1,5 +1,5 @@
 import React, { memo, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { HotRecommendWrapper } from "./style";
 import KBThemeHeaderRCM from "@/components/theme-header-rcm";
 
@@ -8,7 +8,12 @@ export default memo(function KBHotRecommend() {
   //state
   //redux hooks
   const dispatch = useDispatch();
-
+  const { hotRecommends } = useSelector(
+    (state) => ({
+      hotRecommends: state.getIn(["recommend", "hotRecommends"]),
+    }),
+    shallowEqual
+  );
   //other hooks
   useEffect(() => {
     dispatch(getHotRecommendAction(8));
@@ -19,6 +24,11 @@ export default memo(function KBHotRecommend() {
         title="热门推荐"
         keywords={["华语", "流行", "摇滚", "民谣", "电子"]}
       />
+      <div className="recommend-list">
+        {hotRecommends.map((item, index) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
     </HotRecommendWrapper>
   );
 });
