3e5449f41d512f37599589a69bd6f4b92ade0a72
huangkairan <18757128360@163.com>
2020-10-14 22:23:17 +0800

    feat(top-ranking):添加了榜单功能

--- src/pages/discover/c-pages/recommend/c-cpns/ranking/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/ranking/index.js
@@ -1,6 +1,7 @@
 import React, { memo, useEffect } from "react";
 import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import KBThemeHeaderRCM from "@/components/theme-header-rcm";
+import KBTopRanking from "@/components/top-ranking";
 import { KBRankingWrapper } from "./style";
 import { getTopRankingAction } from "../../store/actions";
 export default memo(function KBRanking() {
@@ -10,10 +11,22 @@ export default memo(function KBRanking() {
     dispatch(getTopRankingAction(2));
     dispatch(getTopRankingAction(3));
   }, [dispatch]);
+  const { topRankings, newRankings, originRankings } = useSelector(
+    (state) => ({
+      topRankings: state.getIn(["recommend", "topRankings"]),
+      newRankings: state.getIn(["recommend", "newRankings"]),
+      originRankings: state.getIn(["recommend", "originRankings"]),
+    }),
+    shallowEqual
+  );
   return (
     <KBRankingWrapper>
       <KBThemeHeaderRCM title="榜单" />
-      <div className=""></div>
+      <div className="tops">
+        <KBTopRanking info={topRankings} />
+        <KBTopRanking info={newRankings} />
+        <KBTopRanking info={originRankings} />
+      </div>
     </KBRankingWrapper>
   );
 });
