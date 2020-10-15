5331934dd4dd102b1f506e2b0e96c614b1230d43
huangkairan <18757128360@163.com>
2020-10-15 19:24:10 +0800

    feat:歌词界面结构

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -1,9 +1,18 @@
 import React, { memo } from "react";
-
+import { PlayerWrapper, PlayerLeft, PlayerRight } from "./style";
 export default memo(function KBPlayer() {
   return (
-    <div>
-      <h3>player</h3>
-    </div>
+    <PlayerWrapper>
+      <div className="content wrap-v2">
+          <PlayerLeft className="player-left">
+            <h3>player-info</h3>
+            <h3>song-content</h3>
+          </PlayerLeft>
+          <PlayerRight className="player-right">
+          <h3>simi playlist</h3>
+          <h3>simi song</h3>
+          </PlayerRight>
+      </div>
+    </PlayerWrapper>
   );
 });
