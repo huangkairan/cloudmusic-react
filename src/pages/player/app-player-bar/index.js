b948bff7014967923c0bd29f4973e0bd919999aa
huangkairan <18757128360@163.com>
2020-10-15 18:15:51 +0800

    feat(player):当前进度展示

--- src/pages/player/app-player-bar/index.js
+++ src/pages/player/app-player-bar/index.js
@@ -1,43 +1,74 @@
-import React, { memo, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { memo, useEffect, useRef, useState } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { PlaybarWrapper, Control, PlayInfo, Operator } from "./style";
 import { Slider } from "antd";
 import { getSongDetailAction } from "../store/actions";
+import {
+  getSizeImage,
+  formatMinuteSecond,
+  getPlaySong,
+} from "@/utils/format-utils";
 export default memo(function KBAppPlayerBar() {
+  //state
+  const [currentTime,  setCurrentTime] = useState(0);
+  //redux hooks
   const dispatch = useDispatch();
+  const { currentSong } = useSelector(
+    (state) => ({
+      currentSong: state.getIn(["player", "currentSong"]),
+    }),
+    shallowEqual
+  );
+  //other hooks
+  const audioRef = useRef();
   useEffect(() => {
     dispatch(getSongDetailAction(1452439191));
   }, [dispatch]);
+  //other handle
+  const picUrl = (currentSong.al && currentSong.al.picUrl) || "";
+  const singerName = (currentSong.ar && currentSong.ar[0].name) || "未知歌手";
+  const duration = currentSong.dt || 0;
+  const showDuration = formatMinuteSecond(duration);
+  //handle function
+  const playMusic = () => {
+    audioRef.current.src = getPlaySong(currentSong.id);
+    audioRef.current.play();
+  };
+  const timeUpdate = e =>{
+    setCurrentTime(e.target.currentTime*1000);
+  }
+  const progress = currentTime / duration * 100;
+  const showCurrentTime = formatMinuteSecond(currentTime)
   return (
     <PlaybarWrapper className="sprite_player">
       <div className="content wrap-v2">
         <Control>
           <button className="sprite_player prev"></button>
-          <button className="sprite_player play"></button>
+          <button
+            className="sprite_player play"
+            onClick={(e) => playMusic()}
+          ></button>
           <button className="sprite_player next"></button>
         </Control>
         <PlayInfo>
           <div className="image">
             <a href="/todo">
-              <img
-                src="https://p1.music.126.net/JzsER44sOReoM6mR8XKnsw==/109951165182029540.jpg?param=34y34"
-                alt=""
-              />
+              <img src={getSizeImage(picUrl, 35)} alt="" />
             </a>
           </div>
           <div className="info">
             <div className="song">
-              <span className="song-name">在一起嘛好不好</span>
+              <span className="song-name">{currentSong.name}</span>
               <a href="#/" className="singer-name">
-                李荣浩
+                {singerName}
               </a>
             </div>
             <div className="progress">
-              <Slider defaultValue={30} />
+              <Slider defaultValue={30} value={progress}/>
               <div className="time">
-                <span className="now-time">02:13</span>
+                <span className="now-time">{showCurrentTime}</span>
                 <span className="divider">/</span>
-                <span className="duration">03:45</span>
+                <span className="duration">{showDuration}</span>
               </div>
             </div>
           </div>
@@ -54,6 +85,7 @@ export default memo(function KBAppPlayerBar() {
           </div>
         </Operator>
       </div>
+      <audio ref={audioRef} onTimeUpdate={timeUpdate}/>
     </PlaybarWrapper>
   );
 });
